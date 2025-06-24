b4c8711dc08549bd09159a3002a848f200b34481
emmeiwhite <emmeiwhite@gmail.com>
2025-06-24 09:01:32 +0530

    QueryClient from TanStack Query for ServerSide CRUD operations

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+const url = 'https://dummyjson.com/recipes'
+
+function App() {
+  return (
+    <main className="max-w-6xl mx-auto px-6">
+      <Header />
+
+      <section></section>
+      <Footer />
+    </main>
+  )
+}
+
+export default App
