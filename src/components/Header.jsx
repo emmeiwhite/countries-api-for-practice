aeebde67839acf347067f2dc5fa8c16a2b924271
emmeiwhite <emmeiwhite@gmail.com>
2025-06-25 19:15:14 +0530

    An approach to the project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
+import { LuMoon } from 'react-icons/lu'
+import { LuSun } from 'react-icons/lu'
+
 export default function Header() {
+  const [isDark, setIsDark] = useState(false)
+
+  function handleLightDark() {
+    console.log('handleDarkLight')
+    setIsDark(!isDark)
+  }
   return (
-    <header className="bg-gray-500">
+    <header className="bg-gray-600">
       <div className="max-w-6xl mx-auto px-6 flex justify-between py-4">
         <h1 className="text-xl text-white font-semibold">Where in the World?</h1>
 
-        <button className="text-white">☾ Dark Mode</button>
+        <button
+          className="text-white cursor-pointer text-2xl"
+          onClick={handleLightDark}>
+          {isDark ? <LuSun /> : <LuMoon />}
+        </button>
       </div>
     </header>
   )
