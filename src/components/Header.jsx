1bde4bf2fee97a11499c8a0e6261a1241e82f447
emmeiwhite <emmeiwhite@gmail.com>
2025-06-25 19:05:14 +0530

    Header for new Project - practice makes perfect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,11 @@
 export default function Header() {
-  return <div>Header Component</div>
+  return (
+    <header className="bg-gray-500">
+      <div className="max-w-6xl mx-auto px-6 flex justify-between py-4">
+        <h1 className="text-xl text-white font-semibold">Where in the World?</h1>
+
+        <button className="text-white">☾ Dark Mode</button>
+      </div>
+    </header>
+  )
 }
